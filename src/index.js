109eac33ca72b826709135b1d863c5a822bf4c82
kkukileon305 <kkukileon305@gmail.com>
2022-08-29 11:28:35 +0900

    init

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-import './styles/reset.scss';
-import './styles/common.scss';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledMain = styled.main``;
+
+const Main = () => {
+  return (
+    <StyledMain>
+      <h1>asd</h1>
+    </StyledMain>
+  );
+};
+
+export default Main;
