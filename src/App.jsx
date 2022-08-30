f7682eb8b647daee0e9489c4aa533f0cb33afbed
Bong <won950705@gmail.com>
2022-08-31 00:21:46 +0900

    Add : Archive 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import GlobalStyle from './styles/GlobalStyle';
 import { Routes, Route } from 'react-router-dom';
 import Event from './pages/event/Event';
 import MainPage from './pages/Main';
-import ShopMain from './pages/Shop/ShopMain'
+import ShopMain from './pages/Shop/ShopMain';
+import Archive from './pages/archive/Archive';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import Header from './components/Nav';
@@ -14,10 +15,11 @@ const App = () => {
         <GlobalStyle />
         {/* <Header />  */}
         <Routes>
-          <Route path='/event' element={<Event />} />
           <Route path='/' element={<MainPage />} />
           <Route path='/shop' element={<ShopMain />} />
-					<Route path='/shop/:id' element={<ShopMain />} />
+          <Route path='/shop/:id' element={<ShopMain />} />
+          <Route path='/event' element={<Event />} />
+          <Route path='/archive' element={<Archive />} />
         </Routes>
       </ThemeProvider>
     </>
