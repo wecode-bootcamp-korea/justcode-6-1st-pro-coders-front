818ff3a2d437b0a645038ebd159aa5bcb5d7ee25
kkukileon305 <kkukileon305@gmail.com>
2022-09-06 17:33:11 +0900

    Delete: mock데이터 삭제, Add: Best 스켈레톤 작업중...

--- src/components/Skeleton/BestSkeleton.jsx
+++ src/components/Skeleton/BestSkeleton.jsx
@@ -2,6 +2,13 @@ import styled from 'styled-components';
 
 const StyledSkeleton = styled.section``;
 
-const BestSkeleton = () => <StyledSkeleton></StyledSkeleton>;
+const BestSkeleton = () => (
+  <StyledSkeleton>
+    <div className='left'>
+      <div className='img' />
+    </div>
+    <div className='right'></div>
+  </StyledSkeleton>
+);
 
 export default BestSkeleton;
