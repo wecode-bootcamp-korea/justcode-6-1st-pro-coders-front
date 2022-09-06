2ae6e9cef395c7086ab64d1cec08722fa3a9e9e3
choiseungchul <tmdcjf152@naver.com>
2022-09-06 17:02:48 +0900

    edit : 컴포넌트 위치 이동 및 삭제

--- src/pages/Shop/Shoes/AllShoes.jsx
+++ src/pages/Shop/Shoes/AllShoes.jsx
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 
 import Item from './sections/Item';
-import Nomination from './sections/Nomination';
-import ShopHeader from './sections/ShopHeader';
+
 
 const StyledAllShoes = styled.div``;
 
